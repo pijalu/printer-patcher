3af1219fb34a42dad201a3675aaa16d5c19fc1f2
Pierre Poissinger <pierre.poissinger@gmail.com>
2026-01-21 21:21:36 +0100

    Fix rules to only trigger if a camera is plugged

--- config/scripts/usb-power.sh
+++ config/scripts/usb-power.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
-if [ ! -f /etc/udev/rules.d/50-usb-power.rules ];
-then
-    sudo tee /etc/udev/rules.d/50-usb-power.rules <<EOF
-    ACTION=="add", SUBSYSTEM=="usb", KERNEL=="1-1", TEST=="power/control", ATTR{power/control}="on"
+sudo tee /etc/udev/rules.d/50-usb-power.rules <<EOF
+    ACTION=="add|change", SUBSYSTEM=="usb", ENV{DEVTYPE}=="usb_interface", \
+        ATTR{bInterfaceClass}=="0e", TEST=="../power/control", ATTR{../power/control}="on"
+
     ACTION=="add", SUBSYSTEM=="usb", KERNEL=="2-1", TEST=="power/control", ATTR{power/control}="on"
 EOF
-else
-    echo "* USB Power already installed"
-fi
 
 echo "[OK]"
