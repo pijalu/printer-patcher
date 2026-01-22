9bb2d63d68bcd30fad1a0819b0e4e58f5166747e
Pierre Poissinger <pierre.poissinger@gmail.com>
2026-01-22 18:37:01 +0100

    Updated udev rules

--- config/scripts/usb-power.sh
+++ config/scripts/usb-power.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-sudo tee /etc/udev/rules.d/50-usb-power.rules <<EOF
-    ACTION=="add|change", SUBSYSTEM=="usb", ENV{DEVTYPE}=="usb_interface", \
-        ATTR{bInterfaceClass}=="0e", TEST=="../power/control", ATTR{../power/control}="on"
-
-    ACTION=="add", SUBSYSTEM=="usb", KERNEL=="2-1", TEST=="power/control", ATTR{power/control}="on"
+sudo tee /etc/udev/rules.d/99-usb-power.rules <<EOF
+ACTION=="add", SUBSYSTEM=="usb", ENV{DEVTYPE}=="usb_device", ATTR{idVendor}=="090c", ATTR{idProduct}=="337b",ATTR{power/control}="on"
+ACTION=="add", SUBSYSTEM=="usb", KERNEL=="usb2", ATTR{power/control}="on"
 EOF
 
 echo "[OK]"
